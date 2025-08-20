77a2b510f9aa9fe8c039b98ebb8fb691e4de9b06
Raphael Elias <raphaeleliass@outlook.com>
2025-08-19 23:18:48 -0300

    feat(types): add validatedData field to AppVariables type

--- apps/server/types/appVariables.ts
+++ apps/server/types/appVariables.ts
@@ -4,5 +4,6 @@ export type AppVariables = {
 	Variables: {
 		user: typeof auth.$Infer.Session.user | null;
 		session: typeof auth.$Infer.Session.session | null;
+		validatedData: unknown;
 	};
 };
