75174985de1036ee4fcee2e0897d5dc4039a77e2
Raphael Elias <raphaeleliass@outlook.com>
2025-08-28 12:44:09 -0300

    fix: update build script and enable server start in index.ts

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,12 @@
 import "dotenv/config";
-// import { serve } from "@hono/node-server";
+import { serve } from "@hono/node-server";
 import { app } from "./app";
 
-// serve(
-// 	{
-// 		fetch: app.fetch,
-// 		port: 3000,
-// 	},
-// 	(info) => {
-// 		console.log(`Server is running on http://localhost:${info.port}`);
-// 	},
-// );
-
-export default app;
+serve(
+	{
+		fetch: app.fetch,
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`);
+	},
+);
