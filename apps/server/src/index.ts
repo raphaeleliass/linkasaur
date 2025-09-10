8214699956b2153a764697230906c2e05e16ed32
Raphael Elias <raphaeleliass@outlook.com>
2025-09-10 12:24:02 -0300

    refactor(server): changes on index structure and error middleware to drizzle

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,13 +1,4 @@
 import "dotenv/config";
-import { serve } from "@hono/node-server";
 import { app } from "@/app";
 
-serve(
-	{
-		fetch: app.fetch,
-		port: 3000,
-	},
-	(info) => {
-		console.log(`Server is running on http://localhost:${info.port}`);
-	},
-);
+export default app;
