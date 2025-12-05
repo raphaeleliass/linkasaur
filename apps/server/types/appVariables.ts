09a68c9aa9b22a26fd6d1d6edce4bcfc7489013b
Raphael Elias <raphaeleliass@outlook.com>
2025-12-05 11:16:12 -0300

    remove: removed unused files

--- apps/server/types/appVariables.ts
+++
@@ -1,9 +0,0 @@
-import type { auth } from "@/lib/auth";
-
-export type AppVariables = {
-	Variables: {
-		user: typeof auth.$Infer.Session.user | null;
-		session: typeof auth.$Infer.Session.session | null;
-		validatedData: unknown;
-	};
-};
