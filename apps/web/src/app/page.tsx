1a96d3c9302aa7ce902c01167e03325cf07dddc4
Raphael Elias <raphaeleliass@outlook.com>
2025-12-12 12:41:02 -0300

    feat(web): added home page layout

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,17 @@
+import About from "@/components/about";
+import CallToAction from "@/components/call-to-action";
+import Features from "@/components/features";
+import HeroSection from "@/components/hero-section";
+import Pricing from "@/components/pricing";
+
 export default function page() {
-	return <div>page</div>;
+	return (
+		<main className="">
+			<HeroSection />
+			<Features />
+			<About />
+			<Pricing />
+			<CallToAction />
+		</main>
+	);
 }
