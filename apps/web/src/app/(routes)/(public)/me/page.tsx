db94873464c3779818a92e50ddd67429fe160fbc
Raphael Elias <raphaeleliass@outlook.com>
2025-12-12 12:47:08 -0300

    feat(web): added a button to sign-out

--- apps/web/src/app/(routes)/(public)/me/page.tsx
+++ apps/web/src/app/(routes)/(public)/me/page.tsx
@@ -1,3 +1,27 @@
-export default function page() {
-	return <div>page</div>;
+"use client";
+
+import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { userStore } from "@/store/userStore";
+
+export default function Page() {
+	const { user, setUser } = userStore();
+	return (
+		<main className="">
+			{user && (
+				<Button
+					variant={"destructive"}
+					onClick={async () => {
+						await authClient.signOut();
+						setUser(null);
+						redirect("/");
+					}}
+				>
+					{" "}
+					sair
+				</Button>
+			)}
+		</main>
+	);
 }
